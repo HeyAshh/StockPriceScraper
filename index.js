9a746bd02347caaab77dae0303e2b83c59bd084b
Ashh <Heyashh1@gmail.com>
2020-10-06 01:05:28 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cheerio = require('cheerio');
 (async function() {
     const res = await fetch('https://www.investing.com/equities/tesla-motors');
     const text = await res.text();
-    const found = text.includes('1,417.00');
-    console.log({found});
+    const $ = cheerio.load(text);
+    const price = $('#quotes_summary_current_data span').first().text();
+    console.log({price});
 })
