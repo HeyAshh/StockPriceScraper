6618f3dd4d383ffe5aa0edf8b7133eecda610730
Ashh <Heyashh1@gmail.com>
2020-10-08 01:00:44 +0300

    Add files via upload

---
+++ sheet.js
@@ -0,0 +1,19 @@
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
+module.exports = class Sheet {
+    constructor() {
+        this.doc = new GoogleSpreadsheet('***');
+    }
+    async load() {
+        await this.doc.useServiceAccountAuth(require('./credentials.json'));
+        await this.doc.loadInfo();
+    }
+    async addRows(rows, i) { 
+        const sheet = this.doc.sheetsByIndex[i];
+        await sheet.addRows(rows);
+    }
+    async getRows(i){
+        const sheet = this.doc.sheetsByIndex[i];
+        const rows = await sheet.getRows();
+    }
+}
